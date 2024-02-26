6d4840c5f45dd820bea852123f3fd1ecc298cec0
Otakanutyy <122967332+Otakanutyy@users.noreply.github.com>
2024-02-27 01:15:22 +0600

    Add files via upload

---
+++ lab5/src/app/product-list/product-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Product, products } from '../products';
+import { CategoryService } from '../category.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = [...products];
+  
+
+  ngOnInit(): void {
+    
+  }
+
+  share(product: Product) {
+    const shareUrl = encodeURIComponent(product.link);
+    const telegramShareUrl = `https://t.me/share/url?url=${shareUrl}`;
+
+    // Open the Telegram share link in a new window
+    window.open(telegramShareUrl, '_blank');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
