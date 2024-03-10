1d1b6a91e81d0a0b72665d564bab480d7fb82be1
Islam <kogsaitama@gmail.com>
2024-03-10 13:39:05 +0500

    lab6 create

---
+++ lab6/src/app/album-detail-photos/album-detail-photos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-detail-photos',
+  templateUrl: './album-detail-photos.component.html',
+  styleUrl: './album-detail-photos.component.css'
+})
+export class AlbumDetailPhotosComponent {
+
+}
