a6ee503983d874f91bb68872993ca3b186d7d433
Islam <kogsaitama@gmail.com>
2024-04-03 19:53:37 +0500

    lab8+tasks of lab7

--- lab7/task1/hackerrank/func.py
+++ lab7/task1/hackerrank/func.py
@@ -1,7 +1,6 @@
 def is_leap(year):
     leap = False
     
-    # Write your logic here
     if year%4==0:
         if year%100==0 and year%400==0:
             leap = True
@@ -10,3 +9,4 @@ def is_leap(year):
     else:
         leap = False
     return leap
+print(is_leap(5))
